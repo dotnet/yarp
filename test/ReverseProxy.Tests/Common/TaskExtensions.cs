20a33d682f73adbc88d800c541f817fb7dc1dd91
Miha Zupan <mihazupan.zupan1@gmail.com>
2025-02-11 17:44:52 +0100

    Update license headers

--- test/ReverseProxy.Tests/Common/TaskExtensions.cs
+++ test/ReverseProxy.Tests/Common/TaskExtensions.cs
@@ -1,3 +1,6 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
 // ------------------------------------------------------------------------------
 // <copyright file="TaskExtensions.cs" company="Microsoft Corporation">
 // Copyright (c) Microsoft Corporation. All rights reserved.
