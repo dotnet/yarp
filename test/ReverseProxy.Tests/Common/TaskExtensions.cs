e11cc949802a383002d2e1df93bd3cf7e0d1561f
Miha Zupan <mihazupan.zupan1@gmail.com>
2025-02-11 17:54:03 +0100

    Remove old license header

--- test/ReverseProxy.Tests/Common/TaskExtensions.cs
+++ test/ReverseProxy.Tests/Common/TaskExtensions.cs
@@ -1,12 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-// ------------------------------------------------------------------------------
-// <copyright file="TaskExtensions.cs" company="Microsoft Corporation">
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// </copyright>
-// ------------------------------------------------------------------------------
-
 using System;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
