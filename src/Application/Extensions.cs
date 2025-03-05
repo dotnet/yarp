a7f38f3bc439d5a9013f57c7b0c8503704a66767
Benjamin Petit <bpetit@microsoft.com>
2025-03-05 14:22:37 +0100

    Address comments

--- src/Application/Extensions.cs
+++ src/Application/Extensions.cs
@@ -97,7 +97,7 @@ public static class Extensions
             options.HttpClientFactory = () =>
             {
                 var handler = new HttpClientHandler();
-                handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true;
+                handler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
                 var httpClient = new HttpClient(handler);
                 return httpClient;
             };
@@ -113,24 +113,5 @@ public static class Extensions
             .AddApplicationStatus(tags: ["live"]);
 
         return builder;
-    }
-
-    public static WebApplication MapDefaultEndpoints(this WebApplication app)
-    {
-        // Adding health checks endpoints to applications in non-development environments has security implications.
-        // See https://aka.ms/dotnet/aspire/healthchecks for details before enabling these endpoints in non-development environments.
-        if (app.Environment.IsDevelopment())
-        {
-            // All health checks must pass for app to be considered ready to accept traffic after starting
-            app.MapHealthChecks("/health");
-
-            // Only health checks tagged with the "live" tag must pass for app to be considered alive
-            app.MapHealthChecks("/alive", new HealthCheckOptions
-            {
-                Predicate = r => r.Tags.Contains("live")
-            });
-        }
-
-        return app;
     }
 }
